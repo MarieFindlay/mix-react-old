1ea3782b161589ceac7198e2aaa64eb4f662a254
Marie Findlay <mariealicefindlay@gmail.com>
2018-12-20 20:01:01 +0000

    Refactor to remove repetition

--- src/components/MenuColumn/MenuColumn.js
+++ src/components/MenuColumn/MenuColumn.js
@@ -9,49 +9,26 @@ import "./MenuColumn.scss";
 
 const MenuColumn = props => {
   const cocktails = props.cocktails;
-  if (props.columnView === 1) {
-    return (
-      <div className="cocktailsColumn">
-        <NavBar
-          handleMenuOption1Click={props.handleMenuOption1Click}
-          handleMenuOption2Click={props.handleMenuOption2Click}
-          handleMenuOption3Click={props.handleMenuOption3Click}
-        />
-        <SearchInput />
-        <CocktailsList
-          cocktails={cocktails}
-          handleCocktailClick={props.handleCocktailClick}
-        />
-        <PreviousNextButton />
-      </div>
-    );
-  } else if (props.columnView === 2) {
-    return (
-      <div className="cocktailsColumn">
-        <NavBar
-          handleMenuOption1Click={props.handleMenuOption1Click}
-          handleMenuOption2Click={props.handleMenuOption2Click}
-          handleMenuOption3Click={props.handleMenuOption3Click}
-        />
-      </div>
-    );
-  } else if (props.columnView === 3) {
-    return (
-      <div className="cocktailsColumn">
-        <NavBar
-          handleMenuOption1Click={props.handleMenuOption1Click}
-          handleMenuOption2Click={props.handleMenuOption2Click}
-          handleMenuOption3Click={props.handleMenuOption3Click}
-        />
-        <SearchInput />
-        <CocktailsList
-          cocktails={cocktails}
-          handleCocktailClick={props.handleCocktailClick}
-        />
-        <PreviousNextButton />
-      </div>
-    );
-  }
+
+  return (
+    <div className="cocktailsColumn">
+      <NavBar
+        onMenuOption1Click={props.onMenuOption1Click}
+        onMenuOption2Click={props.onMenuOption2Click}
+        onMenuOption3Click={props.onMenuOption3Click}
+      />
+      {(props.columnView === 1 || props.columnView === 3) && (
+        <>
+          <SearchInput />
+          <CocktailsList
+            cocktails={cocktails}
+            onCocktailClick={props.onCocktailClick}
+          />
+          <PreviousNextButton />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default MenuColumn;
