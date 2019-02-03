40a74819faa61a0ec296ca2d359c2f7e7b794f2f
Marie Findlay <mariealicefindlay@gmail.com>
2019-02-03 16:31:11 +0000

    unignore build files

---
+++ build/precache-manifest.78631ae895acabb3abbd73d0cd5bb85a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "814fe85964ca5752881a6c8e06e3f4f8",
+    "url": "/static/media/cocktails1.814fe859.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "635af9d1f0ed003133e5",
+    "url": "/static/js/main.635af9d1.chunk.js"
+  },
+  {
+    "revision": "1538915b6d2c264db4ee",
+    "url": "/static/js/1.1538915b.chunk.js"
+  },
+  {
+    "revision": "635af9d1f0ed003133e5",
+    "url": "/static/css/main.317de263.chunk.css"
+  },
+  {
+    "revision": "7c3802a79e1c307b270754b7718ffb32",
+    "url": "/index.html"
+  }
+];
