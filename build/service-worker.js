df4c0dee42b7f10237a176473d66087b1bd772dd
Marie Findlay <mariealicefindlay@gmail.com>
2019-02-03 16:58:28 +0000

    prepare to deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78631ae895acabb3abbd73d0cd5bb85a.js"
+  "/MarieFindlay/Mix-React/precache-manifest.909c2b34905e0eff7587eb09f35f7336.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/MarieFindlay/Mix-React/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
